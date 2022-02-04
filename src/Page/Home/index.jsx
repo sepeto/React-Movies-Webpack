c619cac75c299a08ea94297a7369ba7be5e23196
Joseba Portas Abalde <61161487+sepeto@users.noreply.github.com>
2022-02-04 06:58:21 -0500

    Update index.jsx

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -4,7 +4,6 @@ import React, { useEffect } from 'react';
 import ComingSoon from '../../Components/ComingSoon/index.jsx';
 import GridMovies from '../../Components/GridMovies/index.jsx';
 // Styles
-import { ContentHome } from './HomeStyles.js';
 import { Content } from '../../Styles/Globals/ContentPage.js'
 
 const Home = () => {
